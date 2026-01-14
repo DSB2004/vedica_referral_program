b9e363b6837c077e221c77bcd29eab9360ccf9c5
Damanjeet Singh <damanjeetsingh434@gmail.com>
2026-01-15 02:07:33 +0530

    fixed api route and build bug
    Signed-off-by: Damanjeet Singh <damanjeetsingh434@gmail.com>

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -2,11 +2,27 @@
 
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/prisma";
-export default async function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-
     const { referralCode, identifier } = body;
+
+    const check = await db.referralRegistered.findUnique({
+      where: {
+        referralCode_doneByIdentifier: {
+          doneByIdentifier: identifier,
+          referralCode: referralCode,
+        },
+      },
+    });
+
+    if (check)
+      return NextResponse.json(
+        {
+          message: "User already registered with this referral code",
+        },
+        { status: 400 }
+      );
 
     await db.referralRegistered.create({
       data: {
